1e99b5c7d3002ca33ce781ed83d6f805d5199fcb
DIEGO BRAUN DA CUNHA <ALU201830331@uniritter.local>
2019-03-23 10:38:40 -0300

    Created second test

--- spec.js
+++ spec.js
@@ -5,23 +5,28 @@ describe('Tests aula 22/03', () => {
 		browser.get('http://automationpractice.com/index.php');
 	});
 
-	it('Should add item to cart', () => {
+	/*it('Should add item to cart', () => {
 		element(by.css('.blockbestsellers:nth-child(1)')).click();
 		element(by.css('#blockbestsellers > .ajax_block_product:nth-child(1) .replace-2x')).click();
 		element(by.name('Submit')).click();
 		var productAddedElement = element(by.css('h2:nth-child(2)'));
 		var expectedConditions = protractor.ExpectedConditions;
 		browser.wait(expectedConditions.textToBePresentInElement(productAddedElement, 'Product successfully added to your shopping cart', 2000))
-	});
+	});*/
 
 	it('Should block checkout if email not valid', () => {
 		element(by.css('.blockbestsellers:nth-child(1)')).click();
 		element(by.css('#blockbestsellers > .ajax_block_product:nth-child(1) .replace-2x')).click();
 		element(by.name('Submit')).click();
+		var expectedConditions = protractor.ExpectedConditions;
+		var container = element(by.css('.button-container > a'));
+		browser.wait(expectedConditions.elementToBeClickable(container), 2500);
 		element(by.css('.button-container > a')).click();
-		//element(by.css('#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span')).click();
-		//element(by.id('email')).sendKeys('dsadsadsa@aaaa');
-		//element(by.id('passwd')).sendKeys('dsdasdasdd');
-		//element(by.id('SubmitLogin')).click();
-	})
+		element(by.css('#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span')).click();
+		element(by.id('email')).sendKeys('dsadsadsa@aaaa');
+		element(by.id('passwd')).sendKeys('dsdasdasdd');
+		element(by.id('SubmitLogin')).click();
+		var errorElement = element(by.css('.alert ul li, .alert ol li'));
+		browser.wait(expectedConditions.textToBePresentInElement(errorElement, 'Invalid email address.', 1000))
+	});
 });
