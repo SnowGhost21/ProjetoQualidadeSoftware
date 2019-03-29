d02da1dc905ef8715dc082d37f314a07790ad1f3
DIEGO BRAUN DA CUNHA <ALU201830331@uniritter.local>
2019-03-29 19:56:16 -0300

    Update enviroment configs

--- conf.js
+++ conf.js
@@ -1,5 +1,11 @@
 exports.config = {
 	framework: 'jasmine',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs:['spec.js','spec2.js']
+	specs:['spec.js','spec2.js'],
+	capabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+		 args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+	   }
+	}
 }
